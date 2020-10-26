296c7ce437abc905e3f766c5d54abd47b68d39e8
Gelezka20160321\oulyalya <olya.debelaya@gmail.com>
2020-10-26 15:09:35 +0300

    Добавляет актуальную версию сборки

--- build/js/script.js
+++ build/js/script.js
@@ -1,17 +1,19 @@
+'use strict';
+
 (function () {
   var menu = document.querySelector('.main-nav');
   var menuBtn = menu.querySelector('.main-nav__toggle');
 
   var hideMenu = function () {
-    if(menu && menu.classList.contains('main-nav--no-js')) {
+    if (menu && menu.classList.contains('main-nav--no-js')) {
       menu.classList.add('main-nav--closed');
       menu.classList.remove('main-nav--no-js');
     }
-  }
+  };
 
   var toggleMenu = function () {
     menu.classList.toggle('main-nav--closed');
-  }
+  };
 
   hideMenu();
   menuBtn.addEventListener('click', toggleMenu);
