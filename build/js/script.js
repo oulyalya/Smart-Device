79a4001d6a67a1dc615008c438df16f2b332c33b
Gelezka20160321\oulyalya <olya.debelaya@gmail.com>
2020-10-30 00:17:20 +0300

    Актуальная версия сборки

--- build/js/script.js
+++ build/js/script.js
@@ -7,12 +7,14 @@
   var hideMenu = function () {
     if (menu && menu.classList.contains('main-nav--no-js')) {
       menu.classList.add('main-nav--closed');
+      menu.classList.remove('main-nav--open');
       menu.classList.remove('main-nav--no-js');
     }
   };
 
   var toggleMenu = function () {
     menu.classList.toggle('main-nav--closed');
+    menu.classList.toggle('main-nav--open');
   };
 
   hideMenu();
