85de8f0819df1a4341f78ddc914c95b9e7937045
Olga <olya.debelaya@gmail.com>
2020-11-26 00:07:45 +0300

    Создаёт проект, добавляет служебные файлы

--- gulpfile.js
+++ gulpfile.js
@@ -98,4 +98,3 @@ gulp.task("clean", function () {
 });
 
 gulp.task("build", gulp.series("clean", "copy", "css", "images", "webp", "sprite", "html"));
-gulp.task("start", gulp.series("build", "server"));
