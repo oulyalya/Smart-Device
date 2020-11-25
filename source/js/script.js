1186e25d3b5470c3983208d0ebf8b7bf83e375d9
Olga <olya.debelaya@gmail.com>
2020-11-25 23:59:51 +0300

    Создаёт проект

--- source/js/script.js
+++ source/js/script.js
@@ -1,22 +1,22 @@
-'use strict';
+// 'use strict';
 
-(function () {
-  var menu = document.querySelector('.main-nav');
-  var menuBtn = menu.querySelector('.main-nav__toggle');
+// (function () {
+//   var menu = document.querySelector('.main-nav');
+//   var menuBtn = menu.querySelector('.main-nav__toggle');
 
-  var hideMenu = function () {
-    if (menu && menu.classList.contains('main-nav--no-js')) {
-      menu.classList.add('main-nav--closed');
-      menu.classList.remove('main-nav--open');
-      menu.classList.remove('main-nav--no-js');
-    }
-  };
+//   var hideMenu = function () {
+//     if (menu && menu.classList.contains('main-nav--no-js')) {
+//       menu.classList.add('main-nav--closed');
+//       menu.classList.remove('main-nav--open');
+//       menu.classList.remove('main-nav--no-js');
+//     }
+//   };
 
-  var toggleMenu = function () {
-    menu.classList.toggle('main-nav--closed');
-    menu.classList.toggle('main-nav--open');
-  };
+//   var toggleMenu = function () {
+//     menu.classList.toggle('main-nav--closed');
+//     menu.classList.toggle('main-nav--open');
+//   };
 
-  hideMenu();
-  menuBtn.addEventListener('click', toggleMenu);
-})();
+//   hideMenu();
+//   menuBtn.addEventListener('click', toggleMenu);
+// })();
