28132aa6adaca369348ae683f52eb3c1bca3f665
Olga <olya.debelaya@gmail.com>
2020-12-09 02:58:58 +0300

    Настраивает сборку

--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,7 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/js/**/*.js", gulp.series("jscript", "refresh"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
@@ -87,6 +88,15 @@ gulp.task("copy", function () {
     "source/img/**",
     "source/js/**",
     "source//*.ico"
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("jscript", function () {
+  return gulp.src([
+    "source/js/**",
   ], {
     base: "source"
   })
