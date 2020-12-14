50bfaef35c8a9fc09cbd424deae4ff29e86b3093
Olga <olya.debelaya@gmail.com>
2020-12-14 15:02:10 +0300

    Добавляет акутальное состояние проекта

--- source/js/script.js
+++ source/js/script.js
@@ -144,3 +144,58 @@
     return element instanceof Element || element instanceof HTMLDocument;
   }
 })();
+
+(function () {
+  const form = document.querySelector('form');
+  const inputName = form.querySelector('#feedback-name');
+  const inputTel = form.querySelector('#feedback-tel');
+  const textarea = form.querySelector("#feedback-textarea");
+  const submitBtn = form.querySelector('#feedback-submit');
+
+  let isStorageName = true;
+  let isStorageTel = true;
+
+  let storedName = '';
+  let storedTel = '';
+
+  submitBtn.onclick = () => {
+    const username = inputName.value;
+    const tel = inputTel.value;
+
+    if (username && tel) {
+
+      localStorage.setItem('name', username);
+      localStorage.setItem('telephone', tel);
+      console.log("Отправляем форму");
+
+    } else {
+      console.log("Нужно ввести логин и пароль");
+    }
+  }
+
+  inputName.onfocus = () => {
+    try {
+      storedName = localStorage.getItem('name');
+    } catch (err) {
+      isStorageName = false;
+    }
+
+    if (storedName) {
+      inputName.value = localStorage.getItem('name');
+      inputTel.focus();
+    }
+  }
+
+  inputTel.onfocus = () => {
+    try {
+      storedTel = localStorage.getItem('telephone');
+    } catch (err) {
+      isStorageTel = false;
+    }
+
+    if (storedTel) {
+      inputTel.value = localStorage.getItem('telephone');
+      textarea.focus();
+    }
+  }
+})();
